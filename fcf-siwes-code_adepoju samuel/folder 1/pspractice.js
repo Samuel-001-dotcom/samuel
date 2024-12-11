70c716a3c003f22465b70011d3b40bd88b99d7c0
Samuel-001-dotcom <adepojusamuel170@gmail.com>
2024-12-11 12:36:07 -0800

    Add files via upload

---
+++ fcf-siwes-code_adepoju samuel/folder 1/pspractice.js
@@ -0,0 +1,10 @@
+function myproject() {
+    document.getElementById("demo").innerHTML = "Content is now changed!.";
+}
+/*document.getElementById(demo).innerHTML = 5 + 6;
+
+document.write(5 + 6);
+
+alert(5 + 6)
+
+console.log
